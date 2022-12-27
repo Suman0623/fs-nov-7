c4e41b8df8dc84b0aaea0b9c29b265c5f99e72c4
suman0623 <baddikasumanth23@gmail.com>
2022-12-27 10:39:46 +0530

    okie

---
+++ Java/local.java
@@ -0,0 +1,6 @@
+class Test{
+    public static void main(String[] args){
+        int A=100; //local variable//
+       System.out.println(A);  
+    }
+}
---
+++ Java/static.java
@@ -0,0 +1,6 @@
+class Test{
+    static int A=79; //static variable//
+    public static void main(String[] args){
+       System.out.println(A);  
+    }
+}
