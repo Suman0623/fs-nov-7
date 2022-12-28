6e766509c6d45fad7fdd616f74c8963d002d868f
suman0623 <baddikasumanth23@gmail.com>
2022-12-28 13:32:28 +0530

    okie

---
+++ Java/PrimiDT.java
@@ -0,0 +1,20 @@
+class Test{
+    public static void main(String[] args){
+    byte a= 125; 
+    short b=156;
+    int c=256;
+    long d=12434;
+    double e=6.2;
+    float f=10;
+    boolean g=true;
+    char h='s'; 
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+    }
+}
