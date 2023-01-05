34ffa9735a3808ca99b394fb713f6b0f8d403c9d
Suman0623 <baddikasumanth23@gmail.com>
2023-01-05 14:43:40 +0530

    first commit

---
+++ Java/Arthe.java
@@ -0,0 +1,12 @@
+class Arthematic{
+    public static void main(String[] args) {
+       int a=10;
+       int b=26;
+       System.out.println(+a); 
+       System.out.println(+b); 
+       System.out.println(a+b); 
+       System.out.println(a-b); 
+       System.out.println(a%b); 
+       System.out.println(a/b);
+    }
+}
